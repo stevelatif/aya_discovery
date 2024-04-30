fc40092dcfcc6bbee3d452635f8b7e3e157f1565
steve latif <stevelatif@gmail.com>
2024-04-30 15:35:38 -0700

    adding firewall example

---
+++ firewall-002/firewall-002/src/main.rs
@@ -0,0 +1,83 @@
+use anyhow::Context;
+use aya::programs::{Xdp, XdpFlags};
+use aya::{include_bytes_aligned, Bpf};
+use aya_log::BpfLogger;
+use clap::Parser;
+use log::{info, warn, debug};
+use tokio::signal;
+use std::collections::HashMap;
+use aya::maps::HashMap as ayaHashMap;
+use std::net::Ipv4Addr;
+//use figment::{Figment, providers::{Serialized, Yaml, Env, Format}};
+use figment::{Figment, providers::{Yaml, Format}};
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "eth0")]
+    iface: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+    let config: HashMap<String,String> = Figment::new()
+	.merge(Yaml::file("/home/steve/articles/learning_ebpf_with_rust/firewall/firewall-001/config.yaml"))
+	.extract()?;
+
+    println!("Config: {:?}", config);
+
+
+    env_logger::init();
+
+    // Bump the memlock rlimit. This is needed for older kernels that don't use the
+    // new memcg based accounting, see https://lwn.net/Articles/837122/
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {}", ret);
+    }
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/firewall-002"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/firewall-002"
+    ))?;
+    if let Err(e) = BpfLogger::init(&mut bpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+    let program: &mut Xdp = bpf.program_mut("firewall_002").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&opt.iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+
+    // Create a map
+    let mut blocked_ips : ayaHashMap<_,  u32, u8> =
+	ayaHashMap::try_from( bpf.map_mut("BLOCKED_IPS").unwrap())?;
+
+    for (k, v)  in config {
+	//let addr: Ipv4Addr ;
+	if v == "block" {
+	    let addr : Ipv4Addr  = k.parse().unwrap();
+	    println!("addr {:?}" , addr);
+	    let _ = blocked_ips.insert(u32::from(addr), 1, 0);
+	}
+    }
+    // end map
+    
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
